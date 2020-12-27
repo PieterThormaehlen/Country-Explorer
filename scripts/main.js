4072f4b27c6b7e8ade9996282c66b42ec76aa590
Pieter Thormählen <Pieter.Thormaehlen@googlemail.com>
2020-12-28 00:40:06 +0100

    autocomplete initial implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,68 @@
 const countrySelector = document.querySelector('form')
 const countryOutput = document.querySelector('.country')
+const autocompleteOutput = document.querySelector('.autocomplete')
 const baseURL = 'https://restcountries.eu/rest/v2/'
 
-const getCountryData = async (countryName) => {
-	const location = `name/${countryName}`
-	const query = '?fullText=true'
+;(async () => {
+	const location = 'all/'
+	const query = '?fields=name;population'
 	const response = await fetch(baseURL + location + query)
-	const data = await response.json()
-	return data[0]
-}
+	const countries = await response.json()
+	countries.sort((a, b) => b.population - a.population)
 
-countrySelector.addEventListener('submit', async (e) => {
-	e.preventDefault()
-	const countryName = countrySelector.country.value.trim()
-	countrySelector.reset()
-	const countryData = await getCountryData(countryName)
-	countryOutput.innerHTML = `
+	let selectedCountryPosition = 0
+	let positions = 0
+
+	const showAutocomplete = (e) => {
+		autocompleteOutput.removeAttribute('data-hidden')
+		countryOutput.setAttribute('data-hidden', '')
+		const countryName = countrySelector.country.value.trim().toLowerCase()
+		const autocompletedCountries = countries
+			.filter((country) => country.name.toLowerCase().includes(countryName))
+			.sort((a, b) => a.name.toLowerCase().indexOf(countryName) - b.name.toLowerCase().indexOf(countryName))
+			.slice(0, 10)
+		positions = autocompletedCountries.length
+		const countriesFragment = document.createDocumentFragment()
+		autocompletedCountries.forEach((country) => {
+			const article = document.createElement('article')
+			article.textContent = country.name
+			countriesFragment.appendChild(article)
+		})
+		autocompleteOutput.innerHTML = ''
+		autocompleteOutput.appendChild(countriesFragment)
+		autocompleteOutput.children[selectedCountryPosition].setAttribute('data-selected', '')
+	}
+
+	countrySelector.country.addEventListener('focus', showAutocomplete)
+	countrySelector.country.addEventListener('keyup', showAutocomplete)
+
+	countrySelector.country.addEventListener('keydown', (e) => {
+		if (e.keyCode !== 40 && e.keyCode !== 38) return
+		e.preventDefault()
+		if (e.keyCode === 40 && selectedCountryPosition < positions - 1) selectedCountryPosition++
+		if (e.keyCode === 38 && selectedCountryPosition > 0) selectedCountryPosition--
+		showAutocomplete()
+	})
+
+	countrySelector.country.addEventListener('blur', async (e) => {
+		autocompleteOutput.setAttribute('data-hidden', '')
+		countryOutput.removeAttribute('data-hidden')
+	})
+
+	const getCountryData = async (countryName) => {
+		const location = `name/${countryName}`
+		const query = '?fullText=true'
+		const response = await fetch(baseURL + location + query)
+		const data = await response.json()
+		return data[0]
+	}
+
+	countrySelector.addEventListener('submit', async (e) => {
+		e.preventDefault()
+		countrySelector.reset()
+		countrySelector.country.blur()
+		const countryData = await getCountryData(autocompleteOutput.children[selectedCountryPosition].textContent)
+		countryOutput.innerHTML = `
 		<h2>${countryData.name}</h2>`
-})
+	})
+})()
